79706302b116cf3c90c868e2102e46f47bf735d6
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-12-18 20:56:10 -0300

    [DEV] Implementa entidade Role; - Cria relacionamento n-n com User e Role;

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String authority;
+
+    public Role(){
+    }
+
+    public Role(Long id, String authority) {
+        Id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authority);
+    }
+}
