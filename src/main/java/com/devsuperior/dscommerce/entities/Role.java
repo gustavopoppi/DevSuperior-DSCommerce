e44562e4ca9418ae031006ad475c35de6350a2ee
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-12-18 21:07:25 -0300

    [DEV] Adiciona UserDetails e GrantedAuthority;

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +30,7 @@ public class Role {
         Id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
